e7931d0cf3b68db5f8efa93935246ed7b9af490b
vikishooz <elie.jamaa@gmail.com>
2023-05-16 23:38:21 +0200

    ksnServer setup

--- public/client.js
+++ public/client.js
@@ -1,4 +1,11 @@
-const socket = io( 'ws://localhost:5000' );
+let socketUrl;
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+  socketUrl = 'ws://localhost:5000';
+} else {
+  socketUrl = 'ws://ksnserver.onrender.com/';
+}
+
+const socket = io( socketUrl );
 socket.on( "connect", () => {
     console.log( "CONNECTED !" );
     socket.emit( 'numClients' );
