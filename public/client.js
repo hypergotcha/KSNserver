b18a632a53e9397c74c281b3ae3a1e1426f93b54
vikishooz <elie.jamaa@gmail.com>
2023-11-30 10:26:21 +0100

    ok ready

--- public/client.js
+++ public/client.js
@@ -5,11 +5,17 @@ if (window.location.hostname === 'localhost' || window.location.hostname === '12
   socketUrl = 'https://ksnserver.onrender.com:443';
 }
 
-const socket = io( socketUrl );
+var socket = io( socketUrl );
 socket.on( "connect", () => {
     console.log( "CONNECTED !" );
-    socket.emit( 'numClients' );
-    socket.on( 'numClientsResponse' , (data) => {
-        document.getElementById( 'mainText' ).text = data;
+    socket.emit( 'joining' );
+    socket.on( 'communicate' , (data) => {
+        document.getElementById( 'mainText' ).text = JSON.stringify(data);
       });
 } );
+
+setTimeout( sendPing, 1000 );
+function sendPing() {
+  socket.emit( 'pinging', 567 );
+  setTimeout( sendPing, 1000 );
+}
--- server.js
+++ server.js
@@ -3,14 +3,26 @@ const port = 443;
 const express = require( "express" );
 const app = express();
 const http = require( "http" ).createServer(app);
-const io = require( "socket.io" )(http);
+const io = require( "socket.io" )( http,
+    {
+        cors: {
+            origin: "*",
+            credentials: true
+        }
+    } );
 
 app.use( express.static( "public" ) );
 
 io.on( "connect", ( socket ) => {
+
     console.log( 'NEW CONNECTION from ', socket.id, ShowNumClients() );
+
     socket.on( 'disconnect', () => {
         console.log('DISCONNECTION  from: ', socket.id, ShowNumClients() );
+    } );
+
+    socket.on( 'joining', ( data ) => {
+        console.log( "JOINED!" );
     } );
 } );
 
@@ -19,12 +31,20 @@ function updateClients() {
     let seconds = new Date().getSeconds();
 
     if ( process.env.NODE_ENV === 'production' ) {
-        seconds = 'Application is SERVER '+seconds;
+        seconds = 'Application is SERVER NEW '+seconds;
     } else {
         seconds = 'Application is LOCALHOST '+seconds;
     }
  
-    io.emit( 'numClientsResponse', seconds );
+    let centers = [];
+    for ( let i = 0; i < 100; i++ ) {
+        centers[i] = {
+            x: Math.floor( Math.random() * 100 ),
+            y: Math.floor( Math.random() * 100 )
+        };
+    }
+
+    io.emit( 'communicate', centers );
     setTimeout( updateClients, 1000 );
 }
 
@@ -35,8 +55,16 @@ function ShowNumClients() {
 http.listen( port );
  
 if ( process.env.NODE_ENV === 'production' ) {
-    console.log("Application is SERVER");
+    console.log("Application is SERVER OK");
 } else {
     console.log("Application is LOCALHOST");
 }
 
+// Middleware to log incoming messages
+io.use((socket, next) => {
+    socket.onAny( ( eventName, ...args ) => {
+        let time = new Date().toISOString();
+      console.log(`Received event '${eventName}' from ${socket.id}: at ${time}`, args);
+    });
+    next();
+  });
