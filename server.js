d2a021ba5bfe2cca85b8e5aa6dd13650c0e6a591
vikishooz <elie.jamaa@gmail.com>
2023-12-20 20:27:36 +0100

    1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const rateLimit = require("express-rate-limit");
 const app = express();
 const http = require( "http" ).createServer(app);
 const io = require( "socket.io" )( http, {
-    maxHttpBufferSize: 30,
+    maxHttpBufferSize: 300,
     cors: {
         origin: "*",
         credentials: true
